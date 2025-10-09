158a90799693e448f383f185d07d47803fcf34b2
novlan1 <1576271227@qq.com>
2025-10-09 06:35:41 +0000

    Deploying to docs from  @ 7b36b14af324a2e049f30503cb8746351a3ed750 🚀

--- assets/theme-change.CDrowPPt.js
+++
@@ -1,1 +0,0 @@
-import{r as e,ac as a,ad as t,ae as s}from"./index-vOvbSstt.js";const r=()=>{const r=e("light");return a(()=>{r.value=t.theme,s(e=>{r.value=e.theme})}),{theme:r}};export{r as u};
---
+++ assets/theme-change.D_urJIrI.js
@@ -0,0 +1,1 @@
+import{r as e,ac as a,ad as t,ae as s}from"./index-CrzArGZY.js";const r=()=>{const r=e("light");return a(()=>{r.value=t.theme,s(e=>{r.value=e.theme})}),{theme:r}};export{r as u};
